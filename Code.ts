73e873f7471175ee1cbf82b0db6c45f3c64c764d
Walker Little <walker@campstrategic.com>
2022-07-22 14:38:42 -0400

    add function shells

--- Code.ts
+++ Code.ts
@@ -1,3 +1,31 @@
-function init(Sheet sheet) {
+function init() {
   Logger.log("Hello World!")
+}
+
+function winred() {
+  
+}
+
+function getEmail() {
+  
+}
+
+function getSubscribers() {
+  
+}
+
+function checkSubscribers() {
+  
+}
+
+function addSubscriber() {
+  
+}
+
+function serialize() {
+  
+}
+
+function fetchData() {
+  
 }
