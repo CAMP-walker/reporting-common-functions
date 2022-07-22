e36dff6f1e984997b6c03fc0e3660880640ee593
Walker Little <walker@campstrategic.com>
2022-07-22 16:10:13 -0400

    fetch data method outlined

--- Code.ts
+++ Code.ts
@@ -24,8 +24,18 @@ function addSubscriber() {
 
 function serialize() {
   
+  
 }
 
-function fetchData() {
+function fetchData(category: string, query: string, uriComponents: string[]) {
+  var url: string = "https://firepit.campstrategic.com/api8/" +  category + '/' + query;
+  var url = '?' + uriComponents.join('&');
+  const requestData = {
+    method: 'GET',
+    followRedirects: true,
+    muteHttpExceptions: true,
+    contentType: 'application/json'
+  }
   
+  return JSON.parse(UrlFetchApp.fetch(url, requestData).getContentText());
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+# Common functions to be used across reporting spreadsheets
+Todo:
+- [ ] Expose functions for use in existing spreadsheets
+- [ ] Process post requests
+- [ ] Serialize and store processed requests
+- [ ] Pass on any necessary data
+- [ ] Perform user defined get requests
+- [ ] Save spreadsheet resources by doing calculations in code
+- [ ] Replace all existing sheet logic with this new library
